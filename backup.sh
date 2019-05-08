45b0a26c31a43bca0ba305e89c5793f4a2d89492
jinghua.zhou <zhoujinghua@patsnap.com>
2019-05-08 18:42:35 +0800

    add asg_k8s_node_ci_01.json

--- backup.sh
+++ backup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 python dynamodump.py -m backup -r us-east-1 -p zhoujinghuaus -s insights_ans_diversity_2017 --schemaOnly --skipThroughputUpdate
+python dynamodump.py -m backup -r local --host 192.168.3.70 --port 80 --accessKey a --secretKey b -s platform_setting_system_v20170216 --skipThroughputUpdate
